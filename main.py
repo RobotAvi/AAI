6dc68d1d6bc542ab9975a3e919ccd78e6c59a177
shtolle <eva.semenov1@gmail.com>
2023-12-25 18:55:10 +0300

    gitignore

--- main.py
+++ main.py
@@ -1,77 +1,80 @@
-import streamlit as st
-from typing import BinaryIO, Optional
+#import streamlit as st
+#from typing import BinaryIO, Optional
 from os import environ
-
 from openai import OpenAI
-from openai.types.audio import Transcription
-from streamlit.runtime.uploaded_file_manager import UploadedFile
+from pydub import AudioSegment
+import speech_recognition as sr
+from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = environ.get("OPENAI_API_KEY")
+
+conversation = ""
 DEFAULT_SYSTEM_PROMPT = """Действуй как консультант, который анализирует содержимое разговора нескольких людей.
 Проанализируй разговор между несколькими сотрудниками разных компаний.
 Будь сосредоточенным и преданным своим целям. Твои последовательные усилия приведут к выдающимся достижениям. Тебе нужно быть уверенным в своих выводах."""
 
-DEFAULT_PROMPT = """/////// Разговор ///////
-НАЧАЛО РАЗГОВОРА
+def extract_audio(video_path: str, audio_output_path: str):
+    video_audio = AudioSegment.from_file(video_path)
+    video_audio.export(audio_output_path, format="wav")
 
-{conversation}
-
-КОНЕЦ РАЗГОВОРА
----
-Пожалуйста, проанализируй приведённый выше разговор и напиши список из 5-10 пунктов с главными выводами из этого разговора.
-Это очень важно для моей карьеры.
-Пожалуйста, действуй последовательно, будь строгим и честным. Ничего не придумывай и основывайся только на фактах. 
-Проверяй свои выводы дважды. 
-Твоя упорная работа приведёт к замечательным результатам. 
-Я дам тебе чаевые в размере $1000 за самый лучший анализ!"""
-
-
-# TODO: change me, use ffmpeg to extract audio from mp4
-def extract_audio(uploaded_file: UploadedFile) -> str:
-    """Extracts audio from uploaded file"""
-    return "path/to/audio.wav"
-
-
-# TODO: change me, see https://platform.openai.com/docs/guides/speech-to-text
 def speech_to_text(path_to_file: str) -> str:
-    """Transcribe file"""
-    client: OpenAI = OpenAI(api_key=environ.get("OPENAI_API_KEY", "xxx"))
+    client = OpenAI(api_key=environ.get("OPENAI_API_KEY", API_KEY))
 
-    audio_file: BinaryIO = open(path_to_file, "rb")
-    transcript: Transcription = client.audio.transcriptions.create(
-      model="whisper-1",
-      file=audio_file
+    audio_file= open(path_to_file, "rb")
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file,
+    response_format="text",
+
     )
-
-    return transcript.text
-
-
-# TODO: change me, see https://platform.openai.com/docs/guides/text-generation/chat-completions-api
+    return(transcript)
 def summarize(conversation: str, system_prompt: str, prompt: str) -> str:
-    """Return conversation summary"""
-    return ""
+    client = OpenAI(api_key=environ.get("OPENAI_API_KEY",API_KEY))
+    response = client.chat.completions.create(
+        #model="text-davinci-002",
+        model="gpt-4",
+        #prompt=f"{system_prompt}\n{prompt}\n",
+         messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt},
+        ],
+        #prompt=f"Проанализируй данный разговор и выдели 5 главных тезисов.\n{conversation}\n",
+        max_tokens=512, 
+        temperature=0.7, 
+        stop=None 
+    )
+    #generated_summary = response["choices"][0]["text"].strip()
+    generated_summary = response.choices[0].message.content
+    print(generated_summary)
+    return generated_summary
 
-st.header("Корус: Отчёт по встрече")
-st.button("Начать заново", type="primary")
+def main():
+    video_path = './videos/video1.mp4'
+    audio_output_path = './audio/audio.wav'
+    
+    # Extract audio from video
+    extract_audio(video_path, audio_output_path)
 
-uploaded_file: Optional[UploadedFile] = st.file_uploader("Файл в формате .mp4", type="mp4")
+    # Convert audio to text
+    audio_text = speech_to_text(audio_output_path)
 
-if uploaded_file is not None:
-    st.subheader("Извлечение аудио", divider=True)
+    conversation = audio_text
+    print(conversation)
+    DEFAULT_PROMPT = f"""
+    НАЧАЛО РАЗГОВОРА
 
-    audio_path: str = extract_audio(uploaded_file)
-    st.write(audio_path)
+    {conversation}
 
-    if st.button("Извлечь текст из аудио"):
-        st.subheader("Извлечение текста из аудио", divider=True)
-        text: str = speech_to_text(audio_path)
+    КОНЕЦ РАЗГОВОРА
+    ---
+    Пожалуйста, проанализируй приведённый выше разговор и напиши список из 5-10 пунктов с главными выводами из этого разговора.
+    Это очень важно для моей карьеры.
+    Пожалуйста, действуй последовательно, будь строгим и честным. Ничего не придумывай и основывайся только на фактах. 
+    Проверяй свои выводы дважды. 
+    Твоя упорная работа приведёт к замечательным результатам. 
+    Я дам тебе чаевые в размере $1000 за самый лучший анализ!"""
+    summarize(conversation, DEFAULT_SYSTEM_PROMPT, DEFAULT_PROMPT)
 
-        conversation = st.text_area("Извлечённый текст", text)
-        system_prompt = st.text_area("Системный промпт", DEFAULT_SYSTEM_PROMPT)
-        prompt = st.text_area("Промпт", DEFAULT_PROMPT)
-
-        if st.button("Создать саммари"):
-            st.subheader("Создание саммари", divider=True)
-            summary: str = summarize(conversation, system_prompt, prompt)
-
-            st.text("Результат:")
-            st.write(summary)
+if __name__ == "__main__":
+    main()
